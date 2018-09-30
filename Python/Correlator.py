6eb3f5cbdd7b368b4ced288e7c178eb8caab086f
Matt Berglund <berglundma@gmail.com>
2018-09-30 15:12:31 -0400

    Adding error

--- Python/Correlator.py
+++ Python/Correlator.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 # Functions to be imported
 from Equations import *
+from Error import *
 
 # Constants
 g = 9.81
@@ -35,7 +36,7 @@ print(data)
 
 ####### Part 1 #######
 
-### HEM Correlation ###
+### HEM Cororelation ###
 for row_test in data.itertuples():
    D = row_test.D/1000
    t_mdotg = row_test.m_dot_g/1000
@@ -43,16 +44,39 @@ for row_test in data.itertuples():
    gm = G_m(t_mdotg, t_mdotf, D)
    dp_dz = dp_dz_HEM(gm, row_test.mu_f, t_mdotg, t_mdotf, row_test.rho_f, row_test.rho_g, D)
    dp_dz = dp_dz/1000
+   data.at[row_test.Index, 'HEM_dp_dz'] = dp_dz
 
+   #scatter_plot(arrayoftest, arrayofcorr)
    print('.')
    print('.')
    print('.')
    print('HEM Correlation')
-   print('D: %s   m_dot_g: %s   m_dot_f: %s' % (row_test.D, row_test.m_dot_g, row_test.m_dot_f))
+   print(row_test)
+   print('D: %s   m_dot_g: %s   m_dot_f: %s ' % (row_test.D, row_test.m_dot_g, row_test.m_dot_f))
    print('GM: %s' % (gm))
    print('P_test: %s P_data: %s' % (row_test.PressureE, row_test.PressureC))
    print('mu_f: %s   rho_f: %s   rho_g: %s' % (row_test.mu_f, row_test.rho_f, row_test.rho_g))
-   print('Correlated: %s kPa/m  Experimental: %s kPa/m' % (dp_dz, row_test.dP_dz))
+   print('Correlated: %s kPa/m  Experimental: %s kPa/m' % (dp_dz, row_test.dP_dz)
+
+print(data)
+
+# MAE
+mae=MAE(data.HEM_dp_dz, data.dP_dz)
+data.HEM_mae = mae
+
+print(data)
+
+# ME
+merror=MError(data.HEM_dp_dz, data.dP_dz)
+data.HEM_merror = merror
+
+# RMS 
+rms=RMS(data.HEM_dp_dz, data.dP_dz)
+data.HEM_rms = rms
+
+# R2
+r2=R2(data.HEM_dp_dz, data.dP_dz)
+data.HEM_R2 = r2
 
 ### Lockhard-Martinelli Correlation ###
 for row_test in data.itertuples():
@@ -93,7 +117,7 @@ for row_test in data.itertuples():
    print('Correlated: %s kPa/m  Experimental: %s kPa/m' % (dp_dz, row_test.dP_dz))
 
 ####### Part 2 #######
-MAE_LM_Min = 100 # arbitrary starting value
+AE_LM_Min = 100 # arbitrary starting value
 C_min = 0 # arbitrary starting value
 C = 0 # starting value
 while C < 100.01: # ending value
