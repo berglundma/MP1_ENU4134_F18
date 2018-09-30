ca46403f6d43743f8783b5308aff950cff0aa1dc
Vincent Kepner <vincent.kepner@gmail.com>
2018-09-30 14:45:07 -0400

    Fixed Several Minor Errors

--- Python/Error.py
+++ Python/Error.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def R2(corr, exp):
+    sum_total = 0
     n_corr = len(corr)
     n_exp  = len(exp)
 
@@ -10,66 +11,69 @@ def R2(corr, exp):
       exp_mean  = np.mean(exp)
       exp_std   = np.std(exp)
 
-      for x in range(0, n_corr)
-        sum_corr = (corr[x]-corr_mean)/corr_std
-        sum_exp  = (exp[x] -exp_mean)/exp_std
+      for n in range(0, n_corr):
+        sum_corr = (corr[n]-corr_mean)/corr_std
+        sum_exp  = (exp[n]-exp_mean)/exp_std
         sum_total += sum_corr*sum_exp
 
 #     Changes made:
 #     1-n changed to n_corr-1
 #     return r changed to return r**2
-      r = (1/(n_corr-1))(sum_total))
+      r = (1/(n_corr-1))*(sum_total)
       R2 = r**2
       return R2;
 
     else:
-      print "error calculating R2, length of arrays not equal"
+      print('error calculating R2, length of arrays not equal')
 
 # Mean Absolute Error - must be fed 1-D arrays
 def MAE(corr, exp):
+    sum_total = 0
     n_corr = len(corr)
-    n_exp  = len(exp)
+    n_exp = len(exp)
 
     if n_corr == n_exp:
-      for x in range(0, n_corr)
+      for n in range(0, n_corr):
         sum_total += np.absolute(((corr[n]-exp[n])/exp[n])*100)
 
 #     Changes made:
 #     n changed to n_corr
-      mae = (1/n_corr)(sum_total)
+      mae = (1/n_corr)*(sum_total)
       return mae;
 
     else:
-      print "error calculating MAE, length of arrays not equal"
+      print('error calculating R2, length of arrays not equal')
 
 # Root Mean Square error must be fed 1-D arrays
 def RMS(corr, exp):
+    sum_total = 0
     n_corr = len(corr)
     n_exp  = len(exp)
 
     if n_corr == n_exp:
       n = n_corr
-      for x in range(0, n)
+      for n in range(0, n):
         sum_total += (((corr[n]-exp[n])/exp[n])*100)**2
 
-      rms = np.sqrt((1/n)(sum_total)))
+      rms = np.sqrt((1/n)*(sum_total))
       return rms;
 
     else:
-      print "error calculating RMS, length of arrays not equal"
+      print('error calculating R2, length of arrays not equal')
 
 # Mean error must be fed 1-D arrays
 def MEANError(corr, exp):
+    sum_total = 0
     n_corr = len(corr)
     n_exp  = len(exp)
 
     if n_corr == n_exp:
       n = n_corr
-      for x in range(0, n)
+      for n in range(0, n):
         sum_total += ((corr[n]-exp[n])/exp[n])*100
 
-      mean_error = (1/n)(sum_total))
+      mean_error = (1/n)*(sum_total)
       return mean_error;
 
     else:
-      print "error calculating Mean Error, length of arrays not equal"
+      print('error calculating R2, length of arrays not equal')
