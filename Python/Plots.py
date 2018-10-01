7e9aa82a8927bf711a09a80607dab9f0e87f4b9b
Matt Berglund <berglundma@gmail.com>
2018-09-30 21:04:55 -0400

    More error

--- Python/Plots.py
+++ Python/Plots.py
@@ -8,13 +8,12 @@ def scatter_plot(Y_test, Y_pred):
     # Define Plot
     #r_squared = 0.59
     plt.scatter(Y_test,Y_pred)
-    plt.xlabel('Actual values')
-    plt.ylabel('Predicted values')
+    plt.xlabel(r'$\delta P/\delta z$ Measured')
+    plt.ylabel(r'$\delta P/\delta z$ Calculated')
 
     # Generate Plot
-    plt.plot(np.unique(Y_test), np.poly1d(np.polyfit(Y_test, Y_pred, 1))(np.unique(Y_test)))
+    plt.plot(np.unique(Y_test), np.poly1d(np.polyfit(Y_test, Y_pred, 1))(np.unique(Y_test)), color='red')
 
     # Add R-squared label and show/print plot
     #plt.text(0.6, 0.5, 'R-squared = %0.2f' % r_squared)
     plt.show()
-
